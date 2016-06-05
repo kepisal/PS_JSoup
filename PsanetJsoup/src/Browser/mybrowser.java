1fc4734e3daed06b9aeeba04dd8440b15be31e24
kepisal <kepisal@gmail.com>
2016-06-05 17:08:54 +0700

    MyBrowser and IBrowser
    MyBrowser and IBrowser

--- PsanetJsoup/src/Browser/mybrowser.java
+++ PsanetJsoup/src/Browser/mybrowser.java
@@ -1,22 +1,42 @@
 package Browser;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public abstract class mybrowser {
+import branch.IBrowser;
+import utilities.Logger;
+
+public class mybrowser implements IBrowser {
+
+	@Override
+	public Document getDocument(String URL, int timeout) {
+		// TODO Auto-generated method stub
+		try {
+			return Jsoup.connect(URL).timeout(timeout).get();
+		} catch (Exception e) {
+			// TODO: handle exception
+			Logger.writeLogException(e, "getDocument", this.toString());
+		}
+		return null;
+	}
+
+	@Override
+	public Document getDocumentBrowser(String URL, int timeout, String useragent, boolean ignoreContentype,
+			boolean isRedirect) {
+		// TODO Auto-generated method stub
+		try {
+			return Jsoup.connect(URL).userAgent(useragent).ignoreContentType(ignoreContentype)
+					.followRedirects(isRedirect).timeout(timeout).get();
+		} catch (Exception e) {
+			// TODO: handle exception
+			Logger.writeLogException(e, "getDocument", this.toString());
+		}
+		return null;
+	}
 	
-	
-	
-	
-	
-	public abstract Document getDocument(String URL,int timeout);
-	public abstract Document getDocumentBrowser(String URL, int timeout, String useragent, boolean ignoreContentype, boolean isRedirect);
-	public abstract String getTitle();
-	public abstract String getURL();
-	public abstract String getURI();
-	public abstract Elements getElements();
-	public abstract Element getElementsBy(String element);
-	public abstract void getPages();
-	
+	public void StringGrap(){
+		
+	}
 }
