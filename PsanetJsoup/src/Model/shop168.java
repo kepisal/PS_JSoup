5421c76888f2bc4c666bdcd0b66390c37c16b7d2
kepisal <kepisal@gmail.com>
2016-06-14 20:18:53 +0700

    Home
    home

--- PsanetJsoup/src/Model/shop168.java
+++ PsanetJsoup/src/Model/shop168.java
@@ -1,6 +1,8 @@
 package Model;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.mysql.jdbc.log.Log;
 
@@ -27,6 +29,10 @@ public class shop168 extends mybrowser {
 		return getDocumentBrowser(URL, (timeout=timeout<1000?5000:timeout), "Mozilla", true, true);
 	}
 	public static void main(String[] args) {
-		System.out.println(new shop168().getPageShop168().title());
+		/*System.out.println(new shop168().getPageShop168("mobiles-and-tablets-kh/mobile-phones-kh/", 10000)
+				.select("div.product-info"));*/
+		Elements elements=new shop168().getPageShop168().select("div#products_new");
+		elements = elements.select("div.pin_main_container");
+		System.out.println(elements.select("div table.center-block td.product-title-wrap").get(0).text());
 	}
 }
