c14357c852f9a6ed766b074823a677515d155f50
kepisal <kepisal@gmail.com>
2016-06-21 06:51:21 +0700

    t3
    t3

--- PsanetJsoup/src/Model/sabaynews.java
+++ PsanetJsoup/src/Model/sabaynews.java
@@ -16,27 +16,28 @@ public class sabaynews {
 			int i=1;
 			while(true){
 			Document doc=Jsoup.connect("http://news.sabay.com.kh/wp-content/themes/sabaynews_v9.0.1/ajax.php")
-					.data("action","do_action_ajax")
+					//.data("action","do_action_ajax")
 					.data("do_action","moreposts")
-					.data("term_id","3")
-					.data("term_name","cat")
+					//.data("term_id","3")
+					//.data("term_name","cat")
 					.data("paged",Integer.toString(i))
 					.userAgent("Mozilla")
 					.timeout(10000)
-					.ignoreContentType(true)
-					.followRedirects(true)
+					//.ignoreContentType(true)
+					//.followRedirects(true)
 					.post();
 			Thread.sleep(3000);
 			String jsondata = doc.body().text();
-			
-			if(jsondata.contains("msg")){
+			System.out.println(jsondata);
+			break;
+			/*if(jsondata.contains("msg")){
 				break;
 			}else{
 				//JSONArray jsonarray=new JSONArray(jsondata);
 				//System.err.println("page"+i+"+++Content : +++"+jsonarray.length()+"++++++++");
 				System.err.println("page : "+i);
 				i++;
-			}
+			}*/
 			/*JSONArray jsonarray=new JSONArray(jsondata);
 			if(jsonarray.length()>0){
 				System.err.println("++++++++++++++++++++"+i+"+++++++++++++++++++++++");
